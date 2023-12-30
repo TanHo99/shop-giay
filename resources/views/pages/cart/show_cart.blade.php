8e29a6e039be9597e74875b5c9671489bcf03172
nnhann999 <nnhann999@gmail.com>
2023-12-30 21:58:49 +0700

    update show cart view

--- resources/views/pages/cart/show_cart.blade.php
+++ resources/views/pages/cart/show_cart.blade.php
@@ -8,7 +8,7 @@
                     <div class="container-inner">
                         <ul>
                             <li class="home">
-                                <a href="{{URL::to('/trang-chu')}}">Trang chủ</a>
+                                <a href="{{ URL::to('/trang-chu') }}">Trang chủ</a>
                                 <span><i class="fa fa-angle-right"></i></span>
                             </li>
                             <li class="category3"><span>Giỏ hàng</span></li>
@@ -31,57 +31,65 @@
                             $content = Cart::content();
                             ?>
                             <thead>
-                            <tr>
-                                <th>Sản phẩm</th>
-                                <th>Tên sản phẩm</th>
-                                <th>Màu</th>
-                                <th>Kích thước</th>
-                                <th>Giá</th>
-                                <th>Số lượng</th>
-                                <th>Tổng tiền</th>
-                                <th></th>
-                            </tr>
+                                <tr>
+                                    <th>Sản phẩm</th>
+                                    <th>Tên sản phẩm</th>
+                                    <th>Màu</th>
+                                    <th>Kích thước</th>
+                                    <th>Giá</th>
+                                    <th>Số lượng</th>
+                                    <th>Tổng tiền</th>
+                                    <th></th>
+                                </tr>
                             </thead>
                             <tbody>
-                            @foreach($content as $v_content)
-                            <tr>
-                                <td>
-                                    <a href="#"><img src="{{URL::to('public/upload/product/'.$v_content->options->image)}}" class="img-responsive" alt=""/></a>
-                                </td>
-                                <td>
-                                    <h6>{{$v_content->name}}</h6>
-                                </td>
-                                <td>
-                                    <h6>{{$v_content->options->color}}</h6>
-                                </td>
-                                <td>
-                                    <h6>{{$v_content->options->size}}</h6>
-                                </td>
-                                <td>
-                                    <div class="cart-price">{{number_format($v_content->price).' '.'vnđ'}}</div>
-                                </td>
-                                <td class="cart_quantity">
-                                    <div class="cart_quantity_button">
-                                        <form action="{{URL::to('/update-cart-quantity')}}" method="POST">
-                                            {{csrf_field()}}
-                                            <input class="cart_quantity_input" type="text" name="cart_quantity" value="{{$v_content->qty}}">
-                                            <input type="hidden" value="{{$v_content->rowId}}" name="rowId_cart" class="form-control">
-                                            <input type="submit" value="Cập nhật" name="update_qty" class="btn btn-default btn-sm">
-                                        </form>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="cart-subtotal">
-                                            <?php
-                                            $subtotal = $v_content->qty * $v_content->price;
-                                            echo number_format($subtotal).' '.'vnđ';
-                                            ?></div>
-                                </td>
-                                <td class="cart_delete">
-                                    <a class="cart_quantity_delete" href="{{URL::to('/delete-to-cart/'.$v_content->rowId)}}"><i class="fa fa-times"></i></a>
-                                </td>
-                            </tr>
-                            @endforeach
+                                @foreach ($content as $v_content)
+                                    <tr>
+                                        <td>
+                                            <a href="#"><img
+                                                    src="{{ URL::to('public/upload/product/' . $v_content->options->image) }}"
+                                                    class="img-responsive" alt="" /></a>
+                                        </td>
+                                        <td>
+                                            <h6>{{ $v_content->name }}</h6>
+                                        </td>
+                                        <td>
+                                            <h6>{{ $v_content->options->color }}</h6>
+                                        </td>
+                                        <td>
+                                            <h6>{{ $v_content->options->size }}</h6>
+                                        </td>
+                                        <td>
+                                            <div class="cart-price">{{ number_format($v_content->price) . ' ' . 'vnđ' }}
+                                            </div>
+                                        </td>
+                                        <td class="cart_quantity">
+                                            <div class="cart_quantity_button">
+                                                <form action="{{ URL::to('/update-cart-quantity') }}" method="POST">
+                                                    {{ csrf_field() }}
+                                                    <input class="cart_quantity_input" type="text" name="cart_quantity"
+                                                        value="{{ $v_content->qty }}">
+                                                    <input type="hidden" value="{{ $v_content->rowId }}" name="rowId_cart"
+                                                        class="form-control">
+                                                    <input type="submit" value="Cập nhật" name="update_qty"
+                                                        class="btn btn-default btn-sm">
+                                                </form>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div class="cart-subtotal">
+                                                <?php
+                                                $subtotal = $v_content->qty * $v_content->price;
+                                                echo number_format($subtotal) . ' ' . 'vnđ';
+                                                ?></div>
+                                        </td>
+                                        <td class="cart_delete">
+                                            <a class="cart_quantity_delete"
+                                                href="{{ URL::to('/delete-to-cart/' . $v_content->rowId) }}"><i
+                                                    class="fa fa-times"></i></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
@@ -94,7 +102,9 @@
 
                     <!-- Shopping Code -->
                     <div class="shipping coupon hidden-sm">
-                        <div class=""><h5>Discount Codes</h5></div>
+                        <div class="">
+                            <h5>Discount Codes</h5>
+                        </div>
                         <p>Enter your coupon code if you have one.</p>
                         <form>
                             <input type="text" class="coupon-input">
@@ -105,11 +115,14 @@
                     <!-- Shopping Totals -->
                     <div class="shipping coupon cart-totals">
                         <ul>
-                            <li class="cartSubT">Tổng: <span>{{number_format(Cart::subtotal()).' '.'vnđ'}}</span></li>
+                            <li class="cartSubT">Tổng: <span>{{ number_format(Cart::subtotal()) . ' ' . 'vnđ' }}</span>
+                            </li>
                             <li class="cartSubT">Phí ship:<span>0 vnđ</span></li>
-                            <li class="cartSubT">Tổng thành tiền: <span>{{number_format(Cart::subtotal()).' '.'vnđ'}}</span></li>
+                            <li class="cartSubT">Tổng thành tiền:
+                                <span>{{ number_format(Cart::subtotal()) . ' ' . 'vnđ' }}</span>
+                            </li>
                         </ul>
-                        <li><a class="proceedbtn" href="{{URL::to('/show-checkout')}}">Thanh toán</a></li>
+                        <li><a class="proceedbtn" href="{{ URL::to('/show-checkout') }}">Thanh toán</a></li>
                     </div>
                     <!-- Shopping Totals -->
                 </div>
@@ -118,5 +131,4 @@
         </div>
     </div>
     <!-- Shopping Table Container -->
-
 @endsection
